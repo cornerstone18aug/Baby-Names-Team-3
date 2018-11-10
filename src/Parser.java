8bec9df13dc4917300ee25843743b026b1731037
Enrique92 <enrique.alcaide92@gmail.com>
2018-11-09 16:19:13 -0800

    Fixed the problem with the TXT printing

--- src/Parser.java
+++ src/Parser.java
@@ -15,60 +15,69 @@ import java.util.regex.Pattern;
 
 public class Parser {
 
-    private static List<Rating> ratingList;
-    private static List<Rating> ratingListMen;
-    private static List<Rating> ratingListWomen;
+  private static List<Rating> ratingList;
+  private static List<Rating> ratingListMen;
+  private static List<Rating> ratingListWomen;
+  private static int year = 1990;
 
-    public Parser() {
-        ratingList = new ArrayList<>();
-        ratingListMen = new ArrayList<>();
-        ratingListWomen = new ArrayList<>();
+  public Parser() {
+    ratingList = new ArrayList<>();
+    ratingListMen = new ArrayList<>();
+    ratingListWomen = new ArrayList<>();
+  }
+
+  public static String parseString(String path, String charset) {
+    StringBuilder sb = new StringBuilder();
+    String s;
+
+    try (BufferedReader br = new BufferedReader(
+        new InputStreamReader(new FileInputStream(path), charset))) {
+      while ((s = br.readLine()) != null) {
+        sb.append(s);
+      }
+    } catch (java.io.IOException e) {
+      e.printStackTrace();
     }
+    return sb.toString();
+  }
 
-    public static String parseString(String path, String charset) {
-        StringBuilder sb = new StringBuilder();
-        String s;
+  public static List<Rating> parseRating(String text) {
+    Rating ratingObjectMen, ratingObjectWoman;
+    Rating ratingObject;
 
-        try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(new FileInputStream(path), charset))) {
-            while ((s = br.readLine()) != null) {
-                sb.append(s);
-            }
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
-    }
-
-    public static List<Rating> parseRating(String text) {
-      Rating ratingObjectMen, ratingObjectWoman;
-      Rating ratingObject;
-        Pattern pattern = Pattern.compile(
-                "<tr align=\"right\"><td>(?<rating>\\d{1,4})</td><td>(?<nameMan>\\w+)</td><td>(?<nameWoman>\\w+)</td>");
-        Matcher matcher = pattern.matcher(text);
-        while (matcher.find()) {
-            String d = matcher.group("rating");
-            int rating = Integer.parseInt(d);
-            String nameMan = matcher.group("nameMan");
-            String nameWoman = matcher.group("nameWoman");
-          ratingObject = new Rating(rating, nameMan, nameWoman);
-          //ratingObjectWoman = new Rating(rating, nameWoman);
-          //ratingObjectMen = new Rating(rating, nameMan);
-          //ratingListMen.add(ratingObjectMen);
-          //ratingListWomen.add(ratingObjectWoman);
-          //ratingList.addAll(ratingObjectMen, ratingObjectWoman);
-          //ratingList.add(ratingListMen);
-          //ratingList.add(ratingListWomen);
-          ratingList.add(ratingObject);
-        }
-        // Sort the ArrayList
-      //sortArrayListMen(ratingListMen);
-      //sortArrayListWomen(ratingListWomen);
+    Pattern pattern = Pattern.compile(
+        "<tr align=\"right\"><td>(?<rating>\\d{1,4})</td><td>(?<nameMan>\\w+)</td><td>(?<nameWoman>\\w+)</td>");
+    Matcher matcher = pattern.matcher(text);
+    while (matcher.find()) {
+      String d = matcher.group("rating");
+      int rating = Integer.parseInt(d);
+      String nameMan = matcher.group("nameMan");
+      String nameWoman = matcher.group("nameWoman");
+      ratingObject = new Rating(rating, nameMan, nameWoman);
+      //ratingObjectWoman = new Rating(rating, nameWoman);
+      //ratingObjectMen = new Rating(rating, nameMan);
+      //ratingListMen.add(ratingObjectMen);
+      //ratingListWomen.add(ratingObjectWoman);
+      //ratingList.addAll(ratingObjectMen, ratingObjectWoman);
+      //ratingList.add(ratingListMen);
+      //ratingList.add(ratingListWomen);
+      ratingList.add(ratingObject);
+      // We sort the names of the babies before we create the TXT file
       sortArrayList(ratingList);
+      // We create the TXT when we have read all the babies
+      if (rating == 1000) {
         // Create the txt files of the years
         createTXTFiles(ratingList);
-        return ratingList;
+      }
     }
+
+    // Sort the ArrayList
+    //sortArrayListMen(ratingListMen);
+    //sortArrayListWomen(ratingListWomen);
+    // Create the txt files of the years
+    //createTXTFiles(ratingList);
+    return ratingList;
+  }
 
 //    public static List<Rating> sortArrayListMen(List<Rating> ratings) {
 //        Collections.sort(ratings, new Comparator<Rating>() {
@@ -97,34 +106,29 @@ public class Parser {
     return ratings;
   }
 
-    private static boolean createTXTFiles(List<Rating> ratings) {
-        FileWriter flwriter = null;
-        int year = 1990;
-        do {
-            try {
-                flwriter = new FileWriter("babynames/txt/baby" + year + ".txt");
-                BufferedWriter bfwriter = new BufferedWriter(flwriter);
-                bfwriter.write(year + "\n");
-                for (Rating r : ratings) {
-                    bfwriter.write(r.getNameMan() + " " + r.getRating() + "\n");
-                    bfwriter.write(r.getNameWoman() + " " + r.getRating() + "\n");
-                }
-                bfwriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (flwriter != null) {
-                    try {
-                        flwriter.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            year = year + 2;
-        } while (year <= 2008);
-        return true;
+  private static boolean createTXTFiles(List<Rating> ratings) {
+    FileWriter flwriter = null;
+    try {
+      flwriter = new FileWriter("babynames/txt/baby" + year + ".txt");
+      BufferedWriter bfwriter = new BufferedWriter(flwriter);
+      bfwriter.write(year + "\n");
+      for (Rating r : ratings) {
+        bfwriter.write(r.getNameMan() + " " + r.getRating() + "\n");
+        bfwriter.write(r.getNameWoman() + " " + r.getRating() + "\n");
+      }
+      bfwriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (flwriter != null) {
+        try {
+          flwriter.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
     }
+    year = year + 2;
+    return true;
+  }
 }
-
-
